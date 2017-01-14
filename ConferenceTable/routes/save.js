45d5253a518504f7b3e9b6e1e30efbefa64c10e6
刘悦 <liuyue@wanfangdata.com.cn>
2017-01-14 10:21:18 +0800

    sort temp add id

--- ConferenceTable/routes/save.js
+++ ConferenceTable/routes/save.js
@@ -8,7 +8,10 @@ router.get('/', function (req, res) {
     writeFile(file);
     function writeFile(file) {  
         // 测试用的中文  
+        var date = new Date();
+        var time = date.getTime();
         var data = {
+            id: time,
             meetingdate: req.query.meetingdate,
             meetingtt: req.query.meetingtt,
             meetingroom: req.query.meetingroom,
