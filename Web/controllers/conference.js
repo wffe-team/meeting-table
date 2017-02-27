623ec31672da4e23bdb15306a962447cd748cd1b
bishanshan <skdtb@163.com>
2017-02-27 09:53:59 +0800

    update

--- Web/controllers/conference.js
+++ Web/controllers/conference.js
@@ -6,16 +6,24 @@ var ConferenceAccessor = require('../code/conferenceAccessor');
 var accessor = new ConferenceAccessor();
 
 router.get('/save', function (req, res) {
-    accessor.add(new Conference(
-        req.query.meetingtt,
-        req.query.meetinguser,
-        '',
-         new ConferenceTable(231),
-        [req.query.meetingst, req.query.meetinget],
-        req.query.meetingdate
-    )).then(_=> {
-        res.json({ success:true });
-    });
+    var conference = new Conference(
+        req.query.title,
+        req.query.userName,
+        req.query.introduction,
+         new ConferenceTable(req.query.tableRoom),
+        [req.query.startTime, req.query.endTime],
+        req.query.date
+    );
+    //如果有id，则更新
+    if (req.query.id) {
+        accessor.update(conference).then(_=> {
+            res.json({ success: true });
+        });
+    } else {
+        accessor.add(conference).then(_=> {
+            res.json({ success: true });
+        });
+    }
 });
 
 router.get('/delete', function (req, res) {
--- Web/public/src/js/conference.js
+++ Web/public/src/js/conference.js
@@ -1,5 +1,25 @@
 ﻿"use strict";
 
 wf.require('page').render('conference', [], () => {
+    var conferencer = (_=> {
+        var sender = (url, data, callback) => {
+            $.get(url, data, (rsp) => {
+                if ($.isFunction(callback)) {
+                    callback(rsp);
+                }
+            });
+        };
+        return {
+            save: (conference, callback) => {
+
+            },
+            remove: (id, callback) => {
+
+            }
+        }
+    })();
+
+    //保存会议示例，如果有id则为更新
+    //conferencer.save({会议数据}, (rsp) => {});
 
 });
