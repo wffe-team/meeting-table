9fa8511ee6f8935ee453d7d31e2c48d68fd57746
刘悦 <liuyue@wanfangdata.com.cn>
2017-08-18 18:02:40 +0800

    会议室历史按日期排序

--- Web/code/meetingHistory.js
+++ Web/code/meetingHistory.js
@@ -7,9 +7,16 @@ class DataHistory {
     getHistory(days) {
         let meetingStr = fs.readFileSync(file, 'utf8');
         var workDay = new WorkDay(days);
+        let convert = function (date) {
+            let num = date.replace(/-/g,'');
+            return parseInt(num);
+        }
         var result = [];
         if (!meetingStr) { result = [];}
         var data = JSON.parse('[' + meetingStr + ']');
+        data.sort(function (a, b) {
+            return convert(a.date) - convert(b.date);
+        });
         data.forEach((meeting) => {
             if (workDay.days.findIndex(date => date==meeting.date) < 0) {
                 result.push(meeting);
